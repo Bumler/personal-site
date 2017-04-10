780869473e7670f58d98de91d1fb7855ceb4ce65
Bumler <hbulmer@callutheran.edu>
2017-04-09 19:39:39 -0700

    Base layout for my projects is complete
    Carby icon looks pretty rough

--- app/app.js
+++ app/app.js
@@ -23,6 +23,30 @@ mySite.controller('mainController', ['$scope', function($scope){
 	{
 		title: "TheLordDragons.com",
 		picture: "/assets/tld-ico.jpg"
+	},
+	{
+		title: "The System",
+		picture: "/assets/system-ico.png"
+	},
+	{
+		title: "Drone Forensics",
+		picture: "/assets/drone-ico.png"	
+	},
+	{
+		title: "Carby",
+		picture: "/assets/carby-ico.png"	
+	},
+	{
+		title: "V.C. Data Automator",
+		picture: "/assets/badge-ico.png",
+		background: "linear-gradient(to bottom right,#007F00,#004C00)",
+		padding: true	
+	},
+	{
+		title: "Toll Booth Simulator",
+		picture: "/assets/mcm-ico.png",
+		background: "linear-gradient(to bottom right,#BFBFBF,#ABABAB)",
+		padding: true
 	}
 	]
 }]);
