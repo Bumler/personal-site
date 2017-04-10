43b717ff08449338ae8bde45cddb0b30e9853587
Bumler <hbulmer@callutheran.edu>
2017-04-09 18:58:55 -0700

    Created base for project tiles

--- app/app.js
+++ app/app.js
@@ -3,10 +3,26 @@ var mySite = angular.module('mySite', ['ngRoute']);
 mySite.config(['$routeProvider', function($routeProvider){
 	$routeProvider
 	.when('/',{
-		templateUrl: 'views/main.html'
+		templateUrl: 'views/main.html',
+		controller: 'mainController'
 	})
 	.otherwise({
 		redirectTo: '/'
 	});
 
+}]);
+
+mySite.controller('mainController', ['$scope', function($scope){
+	$scope.projects = [
+	{
+		title: "HotMeals.com",
+		picture: "/assets/hotMeals-ico.png",
+		background: "linear-gradient(to bottom right,#c73915,#e7b439)",
+		padding: true
+	},
+	{
+		title: "TheLordDragons.com",
+		picture: "/assets/tld-ico.jpg"
+	}
+	]
 }]);
